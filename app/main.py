135d45cb44ed7fedf0c85acffa7fffbd94f462d8
godfri <godfri@kali>
2025-07-07 19:29:16 +0300

    Add copy_file function

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def copy_file(command):
+def copy_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) != 3 or parts[0] != "cp":
@@ -11,7 +11,8 @@ def copy_file(command):
         return
 
     try:
-        with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
+        with open(source_file, "r") as file_in, \
+                open(destination_file, "w") as file_out:
             file_out.write(file_in.read())
     except FileNotFoundError:
         pass
